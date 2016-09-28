eaaf7c89f07a65424974fb4055aaed5a21054a88
Ramon Silva <ramonss.bque@gmail.com>
2016-09-28 00:00:18 -0300

    Consumindo api da google de previsão do tempo com PHP, feito as validações, falta rodar o projeto

---
+++ PrevTempo.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ramon
+ * Date: 27/09/2016
+ * Time: 22:40
+ */
+class PrevTempo
+{
+    //Função responsável por retornar um array com os parâmetros necessários para a solicitação da previsão do tempo.
+    function Parametros($cidade,$estado,$pais,$idioma){
+        $parametros['cidade'] = $cidade;
+        $parametros['estado'] = $estado;
+        $parametros['pais'] = $pais;
+        $parametros['idioma'] = $idioma;
+        $parametros['urlapi'] = 'http://www.google.com.br/ig/api';
+    }
+
+    //Aqui é passado por parametro retornados na funcao paramtros, para que seja feita a requisição ao google
+    function GeneratePrevTempo($parametros){
+        //Acessando diretamente na internet
+        $url = $parametros['urlapi']."?weather='" . urlencode($parametros['cidade']) ."','" . urlencode($parametros['estado']) . "','" . urlencode($parametros['pais']) . "'&hl=" . $parametros['idioma'];
+
+        //A $url alternativa é usada para que você possa acompanhar esse tutorial de forma offline
+        $url_alternativa = "previsao.xml";
+
+        //Atribui a variavel resultado o conteudo do xml
+        $resultado = file_get_contents($url);
+
+        //Aqui faz a conversão do xml para a variável xml
+        $xml = simplexml_load_string(utf8_encode($resultado));
+
+        //Valores recuperados da string xml, separados em um array multidimensional
+        $dados['info'] = $xml->xpath('/xml_api_reply/weather/forecast_information');
+        $dados['atual'] = $xml->xpath('/xml_api_reply/weather/current_conditions');
+        $dados['info'] = $xml->xpath('/xml_api_reply/weather/forecast_conditions');
+
+        return $dados;
+    }
+}
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include ('./PrevTempo.php');
+
+$objPrevTempo = new PrevTempo();
+//Passando para os parametros os dados.
+$parametros = $objPrevTempo->Parametros('Vitoria', 'Espirito Santo', 'Brazil', 'pt-br');
+//Buscando no google a previsão do tempo
+$objPrevTempo->GeneratePrevTempo($parametros);
+?>
+<html>
+<head>
+    <title>Previsão do tempo com a API do Google Weather</title>
+</head>
+<body>
+<h1>Previsão do tempo - <?php echo date('d/m/Y'), strtotime($previsao['info'][0]->forecast_date['data']); ?></h1>
+<p><b>Cidade: </b> <?php echo utf8_decode($previsao['info'][0]->city['data']); ?> </p>
+
+<h2>O tempo Hoje</h2>
+<table>
+    <tr>
+        <td><img itemprop="image" class="lazy"
+                 src="http://www.google.com<?php echo $previsao['atual'][0]->icon['data']; ?>" alt="clima"/></td>
+        <td><?php echo $previsao['atual'][0]->condition['data']; ?></td>
+    </tr>
+</table>
+
+<h2>Nos próximos dias</h2>
+<table>
+    <?php foreach ($previsao['proximos'] as $item) { ?>
+        <tr>
+            <td><img itemprop="image" class="lazy" src="https://www.google.com<?php echo $item->icon['data']; ?>"
+                     alt="clima"/></td>
+            <td><b><?php echo $item->day_of_week['data']; ?></b> - <?php echo $item->condition['data']; ?>
+                <?php echo $item->low['data'] ?> / <?php echo $item->high['data']; ?></td>
+        </tr>
+    <?php } ?>
+</table>
+</body>
+</html>
